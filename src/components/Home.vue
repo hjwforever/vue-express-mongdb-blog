370a71c6e6f1090eb4c45db25637462f857cf9d5
hjwforever <1326153330@qq.com>
2020-12-06 02:38:58 +0800

    更新主页请求服务器文章的方法

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,36 +1,146 @@
 <template>
-  <div class="flex">
-<!--    <v-parallax :src="require('../assets/bg.jpg')" elevation="18"></v-parallax>-->
+  <div>
+    <!--    <v-parallax :src="require('../assets/bg.jpg')" elevation="18"></v-parallax>-->
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
-          :src="require('../assets/logo.png')"
-          class="my-3"
-          contain
-          height="160"
+            :src="require('../assets/logo.png')"
+            class="my-3"
+            contain
+            height="160"
         />
       </v-col>
+    </v-row>
 
-      <v-col class="mb-4">
-        <h6 class="display-2 font-weight-bold mb-3">
+    <v-row class="text-center">
+      <!-- 如果已登录 -->
+      <v-col v-if="currentUser">
+        <h6 class="display-1 font-weight-bold mb-3" style="user-select: none">
           Hello,  <span class="blue--text">{{ currentUser.username }}</span> !<br />
           Welcome to <a href="https://github.com/hjwforever/vue-express-mongdb-blog" target="_blank">Vue-Express-MongoDB-Blog</a> by <span class="font-italic">HJW</span>
         </h6>
       </v-col>
+      <!-- 如果未登录 -->
+      <v-col v-else class="m-4">
+        <h6 class="display-1 font-weight-bold" style="user-select: none">
+          Hello !<br>
+          Welcome to <a href="https://github.com/hjwforever/vue-express-mongdb-blog" target="_blank">Vue-Express-MongoDB-Blog</a> by <span class="font-italic">HJW</span><br><br>
+          (普通账号(可注册), 不具有删除及修改权限: <a href="#">test</a> 密码:<a href="#">123456</a>) <br>
+          (管理员账号(联系管理员注册), 具有删除及修改权限: <a href="#">hjw</a> 密码:<a href="#">123456</a>)
+        </h6>
+      </v-col>
     </v-row>
+
+    <!-- 拓展列表, 可无限加载服务器中的文章数据, 点击展开查看细节 -->
+    <v-col cols="10" offset="1">
+      <v-expansion-panels focusable popout>
+      <v-app-bar aria-expanded="false" class="v-expansion-panel"  src="https://picsum.photos/2019/1080?random">
+        <v-row no-gutters class="text-warning">
+          <v-col cols="3">
+            <v-card-title>
+              序号 标题
+            </v-card-title>
+          </v-col>
+          <v-col cols="2">
+            <v-card-title>
+              作者
+            </v-card-title>
+          </v-col>
+          <v-col
+              cols="7"
+          >
+            <v-card-title>
+              内容
+            </v-card-title>
+          </v-col>
+        </v-row>
+      </v-app-bar>
+      <v-expansion-panel v-for="(post, index) in posts" :key="post.id" >
+        <v-expansion-panel-header :color="colors[index%colors.length]" class="text-white">
+          <template v-slot:default="{ open }">
+            <v-row no-gutters>
+              <v-col cols="3">
+                {{ index+1 }}.  {{ post.title }}
+              </v-col>
+              <v-col cols="2">
+                <v-fade-transition leave-absolute>
+                  <v-chip v-if="!open" pill dark color="#52c147" >
+                    <v-avatar left>
+                      <v-img src="http://img.aruoxi.top/image/favcionx32.ico"></v-img>
+                    </v-avatar>
+                    {{ post.author }}
+                  </v-chip>
+                </v-fade-transition>
+              </v-col>
+              <v-col
+                  cols="7"
+                  class="text--secondary"
+              >
+                <v-fade-transition leave-absolute>
+                  <span
+                      v-if="!open"
+                      class="text-white"
+                  >
+                    {{ post.content.length>20 ? post.content.substr(0, 20) + "..." : post.content }}
+                  </span>
+                </v-fade-transition>
+              </v-col>
+            </v-row>
+          </template>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content class="mt-4">
+          <PostItem :post="post" :color="colors[index%colors.length]"></PostItem>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+    </v-col>
+    <!-- 随着页面下滚动, 无限从服务器数据库获取数据 -->
+    <infinite-loading @infinite="infiniteHandler"></infinite-loading>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+import PostItem from "@/components/Infinite/PostItem";
+import PostDataService from "@/services/PostDataService";
 
-    data: () => ({
-    }),
-    computed: {
-      currentUser() {
-        return this.$store.state.auth.user;
-      }
+export default {
+  name: 'Home',
+  components: {PostItem,},
+  data: () => ({
+    page: 0,
+    size: 6, //每次加载文章数
+    posts: [],
+    colors: ["#f94144","#f3722c","#f8961e","#f9844a","#f9c74f","#90be6d","#43aa8b","#4d908e","#577590","#277da1"],
+  }),
+  methods: {
+    infiniteHandler($state, page, size) {
+      const params= {
+              page: page||this.page,
+              size: size||this.size,
+            };
+      PostDataService.getAll(params).
+        then(({ data }) => {
+          if (data.posts.length) {
+            this.page += 1;
+            // console.log(data);
+            // console.log(data.posts);
+            this.posts.push(...data.posts);
+            $state.loaded();
+          } else {
+            $state.complete();
+          }
+        });
     },
-  }
+  },
+  computed: {
+    currentUser() {
+      let user = this.$store.state.auth.user;
+      if(!user){
+        this.$send('暂无发表或管理文章的权限,请登录或注册', 'error', 10000);
+
+      }
+      return user;
+    }
+  },
+}
 </script>
