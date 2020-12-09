6dd02581788be5dd104bc842bbc8084f43c4a401
hjwforever <1326153330@qq.com>
2020-12-09 12:15:24 +0800

    删除不必要的引用

--- src/main.js
+++ src/main.js
@@ -4,25 +4,20 @@ import router from '@/router/index';
 import store from './store';
 import VeeValidate from 'vee-validate';
 import InfiniteLoading from "vue-infinite-loading";
+
+// vuetify, 本项目主要使用的UI框架
 import vuetify from './plugins/vuetify';
-import VueSweetalert2 from 'vue-sweetalert2'
 
 // Infinite组件(页面滚动无限加载模块) 自定义全局样式
 import NoMore from "@/components/Infinite/NoMore";
 import NoResults from "@/components/Infinite/NoResults";
 import Error from "@/components/Infinite/Error";
-// 引入样式
+
+// 确认框及其样式
+import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
 
 Vue.config.productionTip = false;
-
-// 使用样式
-Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 // 表单验证规则
 Vue.use(VeeValidate);
@@ -39,7 +34,7 @@ Vue.use(InfiniteLoading, {
   }
 })
 
-//确认框
+// 确认框
 Vue.use(VueSweetalert2);
 
 // 全局注册封装好的信息提示条的消息发送方法
