d445c7379415ab494bbcd5cfa76dc3c3c5000b23
hjwforever <1326153330@qq.com>
2020-12-09 12:20:06 +0800

    针对项目部署的更新, 修改api基路由并将build文件夹加入git

--- src/http.js
+++ src/http.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/api",
-    // baseURL: "http://domain/api",
+    // baseURL: "http://localhost:8080/api",
+    baseURL: "http://www.blog.aruoxi.top/api",
     headers: {
         "Content-type": "application/json"
     }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth/';
-// const API_URL = 'http://domain/api/auth/';
+// const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = 'http://www.blog.aruoxi.top/api/auth/';
 
 class AuthService {
     login(user) {
