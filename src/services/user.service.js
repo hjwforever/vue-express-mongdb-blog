1495aff5a0c8d731ff892326b813b00a7b87cce4
hjwforever <1326153330@qq.com>
2020-12-09 12:20:06 +0800

    针对项目部署的更新, 修改api基路由并将build文件夹加入git

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/api/test/';
-// const API_URL = 'http://domain/api/test/';
+// const API_URL = 'http://localhost:8080/api/test/';
+const API_URL = 'http://www.blog.aruoxi.top/api/test/';
 
 class UserService {
     getPublicContent() {
