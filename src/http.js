13e0b2f742163b0e5d9428e37c0724c909adffe0
hjwforever <1326153330@qq.com>
2020-12-10 00:04:07 +0800

    update

--- src/http.js
+++ src/http.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "http://localhost:8080/api",
-    baseURL: "http://www.blog.aruoxi.top/api",
+    // baseURL: "http://localhost:8080/api",    // 提交作业中的server后端整合的是这个, 即前端访问本地服务器接口
+    baseURL: "http://www.blog.aruoxi.top/api",  // 只运行client前端会访问 已部署于阿里云服务器的后端api接口
     headers: {
         "Content-type": "application/json"
     }
