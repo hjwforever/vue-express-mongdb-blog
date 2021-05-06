f395d55de387e89c48935f7f07fadc2f750dd944
hjwforever <hjwbelieve@foxmail.com>
2021-05-06 23:01:17 +0800

    update

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = process.env.VUE_APP_BASEAPI + "/auth/";
+// const API_URL = 'http://localhost:8080/api/auth/';
 // const API_URL = 'http://domain/api/auth/';
 
 class AuthService {
