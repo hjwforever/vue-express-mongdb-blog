da28d1a9134c3daae1f7b0adaacb130fa17b488a
hjwforever <hjwbelieve@foxmail.com>
2021-05-06 23:01:39 +0800

    update

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: process.env.VUE_APP_BASEAPI,
     // baseURL: "http://domain/api",
     headers: {
         "Content-type": "application/json"
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/api/test/';
+const API_URL = process.env.VUE_APP_BASEAPI + "/test/";
+// const API_URL = 'http://localhost:8080/api/test/';
 // const API_URL = 'http://domain/api/test/';
 
 class UserService {
