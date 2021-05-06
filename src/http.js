aa73eb4b869a2cf0c43483703168a177d94e801a
hjwforever <hjwbelieve@foxmail.com>
2021-05-06 23:01:39 +0800

    update

--- src/http.js
+++ src/http.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "http://localhost:8080/api",    // 提交作业中的server后端整合的是这个, 即前端访问本地服务器接口
-    baseURL: "https://blog-apis.aruoxi.top/api",  // 只运行client前端会访问 已部署于云服务器的后端api接口
+    baseURL: process.env.VUE_APP_BASEAPI,
+    // baseURL: "http://domain/api",
     headers: {
         "Content-type": "application/json"
     }
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-// const API_URL = 'http://localhost:8080/api/test/';   // 提交作业中的server后端整合的是这个, 即前端访问本地服务器接口
-const API_URL = 'https://blog-apis.aruoxi.top/api/test/'; // 只运行client前端会访问 已部署于阿里云服务器的后端api接口
+const API_URL = process.env.VUE_APP_BASEAPI + "/test/";
+// const API_URL = 'http://localhost:8080/api/test/';
+// const API_URL = 'http://domain/api/test/';
 
 class UserService {
     getPublicContent() {
