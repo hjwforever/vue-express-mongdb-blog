561e7fde95f93743f05db28ef92ad9ee4a524682
Junwen Huang <hjwbelieve@gmail.com>
2022-03-24 22:08:21 +0800

    update api address

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:8080/api/auth/';   // 提交作业中的server后端整合的是这个, 即前端访问本地服务器接口
-const API_URL = 'http://www.blog.aruoxi.top/api/auth/'; // 只运行client前端会访问 已部署于阿里云服务器的后端api接口
+const API_URL = 'https://blog-apis.aruoxi.top/api/auth/'; // 只运行client前端会访问 已部署于阿里云服务器的后端api接口
 
 class AuthService {
     login(user) {
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from './auth-header';
 
 // const API_URL = 'http://localhost:8080/api/test/';   // 提交作业中的server后端整合的是这个, 即前端访问本地服务器接口
-const API_URL = 'http://www.blog.aruoxi.top/api/test/'; // 只运行client前端会访问 已部署于阿里云服务器的后端api接口
+const API_URL = 'https://blog-apis.aruoxi.top/api/test/'; // 只运行client前端会访问 已部署于阿里云服务器的后端api接口
 
 class UserService {
     getPublicContent() {
